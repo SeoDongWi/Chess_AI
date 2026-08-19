14ece768b66ab4ecbc65435e92d97a7fceed6542
SeoDongWi <masdfg2@naver.com>
2026-08-19 16:32:55 +0900

    M3 - ISSUE26 FIN

--- third_milestone.py
+++ third_milestone.py
@@ -1,5 +1,4 @@
 import chess
-import csv
 import time
 import pandas as pd
 from pathlib import Path
@@ -116,9 +115,126 @@ def make_csv_file(game_results):
 
     loaded_dataframe = pd.read_csv(csv_path)
 
-    print(loaded_dataframe.head())
+    return loaded_dataframe
 
+def make_ai_data_csv(data):
 
+    base_dir = Path(__file__).resolve().parent
+    data_dir = base_dir / "data" / "results"
+    csv_path = data_dir / "ai_results.csv"
+    data_dir.mkdir(parents=True,exist_ok=True)
+
+    data.to_csv(
+        csv_path,
+        index=False,
+        encoding="utf-8-sig",
+        mode="w",
+    )
+ 
+def calculate_performance_statistics(data):
+
+    data = data.copy()
+
+    data["winner"] = "draw"
+    data["loser"] = "draw"
+    white_win = data["result"] == "1-0"
+    black_win = data["result"] == "0-1"
+
+    data.loc[white_win, "winner"] = data.loc[white_win, "white"]
+    data.loc[white_win, "loser"] = data.loc[white_win, "black"]
+
+    data.loc[black_win, "winner"] = data.loc[black_win, "black"]
+    data.loc[black_win, "loser"] = data.loc[black_win, "white"]
+
+    win_counts = data["winner"].value_counts()
+    loss_counts = data["loser"].value_counts()
+    draw_count = (data["result"] == "1/2-1/2").sum()
+    total_games = len(data)
+
+    average_game_length = (
+        (data["moves_count"] + 1) // 2
+    ).mean()
+
+    white_search = data[
+        ["white", "white_search_time", "white_moves_count"]
+    ].rename(columns={
+        "white" : "ai",
+        "white_search_time" : "search_time",
+        "white_moves_count" : "move_count",
+    })
+
+    black_search = data[
+        ["black", "black_search_time", "black_moves_count"]
+    ].rename(columns={
+        "black" : "ai",
+        "black_search_time" : "search_time",
+        "black_moves_count" : "move_count",
+    })
+
+    all_search = pd.concat([white_search, black_search], ignore_index=True,)
+
+    ai_search_groupby = all_search.groupby(
+        "ai",
+        as_index=False,
+    ).agg(
+        total_search_time=("search_time", "sum"),
+        total_move_count=("move_count", "sum"),
+    )
+
+    ai_search_groupby["average_search_time"] = (
+        ai_search_groupby["total_search_time"] / ai_search_groupby["total_move_count"]
+    )
+
+    ai_search_groupby["average_search_time"] = (
+        ai_search_groupby["average_search_time"].round(6)
+    )
+
+    ai_search_groupby["total_search_time"] = (
+        ai_search_groupby["total_search_time"].round(6)
+    )
+
+    ai_performance = ai_search_groupby.copy()
+
+    ai_performance["wins"] = (
+        ai_performance["ai"].map(win_counts).fillna(0).astype(int)
+    )
+
+    ai_performance["draws"] = draw_count
+
+    ai_performance["losses"] = (
+        ai_performance["ai"].map(loss_counts).fillna(0).astype(int)
+    )
+
+    ai_performance["win_rate"] = (
+        ai_performance["wins"] / total_games * 100
+    ).round(2)
+
+    ai_performance["draw_rate"] = (
+        ai_performance["draws"] / total_games * 100
+    ).round(2)
+
+    ai_performance["loss_rate"] = (
+        ai_performance["losses"] / total_games * 100
+    ).round(2)
+
+    ai_performance["average_game_length"] = round(
+    average_game_length, 2
+    )
+
+    ai_performance = ai_performance[[
+        "ai",
+        "wins",
+        "draws",
+        "losses",
+        "win_rate",
+        "draw_rate",
+        "loss_rate",
+        "average_game_length",
+        "average_search_time",
+        "total_search_time",
+    ]]
+
+    return ai_performance
 if __name__ == "__main__":
     game_results = []
     
@@ -128,4 +244,6 @@ if __name__ == "__main__":
             game_results.append(play_chess_without_user(random_ai, minimax_ai, board, False))
         else:
             game_results.append(play_chess_without_user(minimax_ai, random_ai, board, False))
-    make_csv_file(game_results)
+    data = make_csv_file(game_results)
+    ai_data = calculate_performance_statistics(data)
+    make_ai_data_csv(ai_data)
